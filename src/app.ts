e0713256ad6f32344294e80895e287728c26a009
VirtualWolf <virtualwolf@virtualwolf.org>
2024-04-10 22:02:21 +1000

    Added note that the Powerwall URL must be HTTPS and not HTTP.

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,12 @@ import { getPowerwallData } from './lib/get-powerwall-data';
 import { uploadToPvoutput } from './lib/upload-to-pvoutput';
 import logger from './logger';
 
+if (!process.env.POWERWALL_URL?.match(/^https:\/\//)) {
+    logger('POWERWALL_URL environment must be HTTPS, not HTTP. Exiting.', 'ERROR');
+
+    process.exit(1);
+}
+
 logger('App started!');
 
 // Save the data from the Powerwall every five seconds
